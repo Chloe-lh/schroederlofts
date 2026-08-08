e7d7b737d39f285308b7c87cbf64eb8019089069
Chloe Haynes <chloehaynes5252@gmail.com>
2026-08-08 11:17:08 -0600

    airbnb listing

--- src/app/components/airbnb.tsx
+++ src/app/components/airbnb.tsx
@@ -1,4 +1,6 @@
 import Script from "next/script";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Airbnb() {
   return (
@@ -8,8 +10,8 @@ export default function Airbnb() {
         strategy="afterInteractive"
       />
 
-      <section className="mx-auto max-w-3xl py-12 text-center bg-white">
-        <h2 className="text-3xl font-semibold">We're also on Airbnb</h2>
+      <section className="airbnb-section mx-auto max-w-3xl py-6">
+        <h2 className="text-5xl font-semibold">We're also on Airbnb</h2>
 
         <p className="mt-4 text-lg">
           Check out verified reviews from our previous guests.
@@ -21,20 +23,27 @@ export default function Airbnb() {
           possible.
         </p>
 
-        <div
-          className="airbnb-embed-frame mt-8 mx-auto"
-          data-id="1280568463197170078"
-          data-view="home"
-          data-hide-price="true"
-          style={{
-            width: "450px",
-            height: "300px",
-          }}
-        >
-          <a href="https://www.airbnb.ca/rooms/1280568463197170078">
-            View on Airbnb
-          </a>
+
+      <div className="mx-auto mt-10 w-fit p-6 rounded-4xl shadow bg-white">
+        <Image
+          src="/images/airbnb-listing.png"
+          alt="Our Airbnb listing"
+          width={500}
+          height={200}
+          className="airbnb-image"
+        />
         </div>
+
+      
+      <div className="mt-10 text-center">
+        <Link
+          href="https://www.airbnb.ca/rooms/1280568463197170078"
+          target="_blank"
+          className="inline-block rounded-lg bg-[#FF5A5F] px-8 py-3 text-white transition hover:opacity-90"
+        >
+          View Our Airbnb Listing
+        </Link>
+      </div>
       </section>
     </>
   );
